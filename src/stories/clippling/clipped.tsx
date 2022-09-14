ff6af8c1ef62e17fae21af3643976781e5d2d508
Huxley <huxley@Huxleys-MBP.fritz.box>
2022-09-14 15:20:48 +0200

    clipping an origami bird

---
+++ src/stories/clippling/clipped.tsx
@@ -0,0 +1,21 @@
+import './clipped.css';
+
+export const ClippedBird = () => {
+ return (
+    <div className="backdrop"> 
+        <div className="feather" id='first'></div>
+        <div className="feather" id='second'></div>
+        <div className="feather" id='third'></div>
+        <div className="feather" id='fourth'></div>
+        <div className="feather" id='fifth'></div>
+        <div className="feather" id='sixth'></div>
+        <div className="feather" id='seventh'></div>
+        <div className="feather" id='eighth'></div>
+        <div className="feather" id='ninth'></div>
+        <div className="feather" id='tenth'></div>  
+     
+
+
+    </div>
+ )
+} 
