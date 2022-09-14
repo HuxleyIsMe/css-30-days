d1963c8c58d28c55f6cd663f487b9846be3b6057
Huxley <huxley@huxleys-mbp.localdomain>
2022-09-14 16:36:32 +0200

    style: add tweaking details

--- src/stories/clippling/clipped.tsx
+++ src/stories/clippling/clipped.tsx
@@ -2,6 +2,8 @@ import './clipped.css';
 
 export const ClippedBird = () => {
  return (
+    <div className='main'>
+        <h1>hover on me</h1>
     <div className="backdrop"> 
         <div className="feather" id='first'></div>
         <div className="feather" id='second'></div>
@@ -14,6 +16,7 @@ export const ClippedBird = () => {
         <div className="feather" id='ninth'></div>
         <div className="feather" id='tenth'></div>  
         {/* <div  id='eleventh'></div>   */}
+    </div>
     </div>
  )
 } 
