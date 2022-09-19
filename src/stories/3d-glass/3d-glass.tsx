1e69a45b0f944be86a823ec5a605133895d8ee7b
Huxley <huxley@Huxleys-MacBook-Pro.local>
2022-09-19 14:05:00 +0200

    feat: new phone with layers

---
+++ src/stories/3d-glass/3d-glass.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './3d-glass.css'
+
+export const Glass = () => (
+    <div className='wrapper'>
+
+
+        <div className='phone-container'>
+            <div className="phone screen"></div>
+            <div className="oled screen"></div>
+            <div className='touch-panel screen'></div>
+            <div className='polarizer screen'></div>
+            <div className='glass screen'></div>
+        </div>
+
+    </div>
+
+)
