d1fb1ce81a5702cf7da4cc97e319b1db4f511e48
Huxley <huxley@Huxleys-MacBook-Pro.local>
2022-09-19 15:02:09 +0200

    style: tifying of phone css

--- src/stories/3d-glass/3d-glass.tsx
+++ src/stories/3d-glass/3d-glass.tsx
@@ -7,10 +7,42 @@ export const Glass = () => (
 
         <div className='phone-container'>
             <div className="phone screen"></div>
+
             <div className="oled screen"></div>
+
             <div className='touch-panel screen'></div>
+
             <div className='polarizer screen'></div>
+
             <div className='glass screen'></div>
+
+        </div>
+        <div className='tags'>
+            <div className='pointers'>
+
+                <div></div>
+
+                <div></div>
+
+                <div></div>
+
+                <div></div>
+            </div>
+            <div className='labels'>
+                <p>glass</p>
+
+                <p>polarizer</p>
+
+                <p>touch panel</p>
+
+                <p>OLED display</p>
+
+            </div>
+
+
+
+
+
         </div>
 
     </div>
